212b84e1638006217d92f2fd72c26271ee7b54ef
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-18 19:30:37 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
  // classe Cliente.
- 
+  
 public class Cliente {
 	
 	//Atributos
@@ -11,25 +11,67 @@ public class Cliente {
 		private String endereco;
 		private int dataDeNascimento;
 		private int telefone;
-		
+		private String senha;
+		private String login;
+
 		Scanner entrada = new Scanner(System.in);
 		
 		
-           // construtor
+		//contrutor de Cliente
 		public Cliente( String cpf, String email,String endereco, int dataDeNascimento, int telefone ){
 			this.cpf= cpf;
 			this.email= email;
 			this.endereco=endereco;
 			this.dataDeNascimento=dataDeNascimento;
 			this.telefone=telefone;
-			
+			this.login=login;
+			this.senha=senha;
+
 			
 		}
+		
+		
 		//  dados do cliente
 		String getDados(){
 			return " Dados do cliente:\n\n CPF: " + this.cpf + "\n Email: " + this.email + "\n Endereço: " +this.endereco+" \n Data de Nascimento: " + this.dataDeNascimento + "\n Telefone: " + this.telefone ;
-		}	
-}		
+		}
+		
+		// login do cliente
+		 public String getLogin(){
+			return this.login;
+		}
+		
+		// senha do cliente
+		 public	String getSenha(){
+				return this.senha;
+			}
+		 void setcadastrarCliente(String cpf){
+				System.out.println("Digite seu  cpf: ");
+				String c= entrada.next();
+				this.cpf= c;	
+		 }
+
+
+
+}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
