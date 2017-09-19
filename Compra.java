9639e01f3b56f0ca5af5a39594e1a1af94b46235
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-18 23:02:18 -0300

    Add files via upload

---
+++ Compra.java
@@ -0,0 +1,150 @@
+
+public class Compra {
+
+	//Atributos
+			private Double valorUnitario;
+			private Double valorTotal;
+			private int quantidade;
+			
+			
+			public Compra(Double valorUnitario, Double valorTotal, int quantidade){
+				this. valorUnitario=  valorUnitario;
+				this.valorTotal = valorTotal;
+				this.quantidade = quantidade;
+			}
+			
+		//  Dados  da compra
+			String getDados(){
+				return "Dados da compra:\n \nvalor unitário: " + this.valorUnitario + "\n valorTotal: " + this.valorTotal + "\nquantidade" + this.quantidade ;
+			}	
+		
+			
+			
+			//Metados
+			void valorUnitario(){
+				
+			}
+		    void valorTotal (){
+				
+			}
+		    
+		    void quantidade(){
+		    	
+		    }
+		    	
+}		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    }
