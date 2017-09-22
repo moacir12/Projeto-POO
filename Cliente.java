c7dde1d4d8e5033324f64feed6420c6817edc05a
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-22 20:38:06 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,58 +1,69 @@
-//Autor: Moacir José
+	
+	// Autor: Moacir José
 import java.util.Scanner;
 
- // classe Cliente.
-  
 public class Cliente {
-	
-	//Atributos
-		private String cpf;
-		private String email;
-		private String endereco;
-		private int dataDeNascimento;
-		private int telefone;
-		private String senha;
-		private String login;
 
-		Scanner entrada = new Scanner(System.in);
-		
-		
-		//contrutor de Cliente
-		public Cliente( String cpf, String email,String endereco, int dataDeNascimento, int telefone ){
-			this.cpf= cpf;
-			this.email= email;
-			this.endereco=endereco;
-			this.dataDeNascimento=dataDeNascimento;
-			this.telefone=telefone;
-			this.login=login;
-			this.senha=senha;
+	// Atributos
+	private String cpf;
+	private String email;
+	private String endereco;
+	private int dataDeNascimento;
+	private int telefone;
+	private String login;
+	private int senha;
 
-			
-		}
-		
-		
-		//  dados do cliente
-		String getDados(){
-			return " Dados do cliente:\n\n CPF: " + this.cpf + "\n Email: " + this.email + "\n Endereço: " +this.endereco+" \n Data de Nascimento: " + this.dataDeNascimento + "\n Telefone: " + this.telefone ;
-		}
-		
-		// login do cliente
-		 public String getLogin(){
-			return this.login;
-		}
-		
-		// senha do cliente
-		 public	String getSenha(){
-				return this.senha;
-			}
-		 void setcadastrarCliente(String cpf){
-				System.out.println("Digite seu  cpf: ");
-				String c= entrada.next();
-				this.cpf= c;	
-		 }
+	// contrutor de Cliente
+	public Cliente(String cpf, String email, String endereco,
+			int dataDeNascimento, int telefone, String login, int senha) {
+		this.cpf = cpf;
+		this.email = email;
+		this.endereco = endereco;
+		this.dataDeNascimento = dataDeNascimento;
+		this.telefone = telefone;
+		this.login = login;
+		this.senha = senha;
+	}
 
+	public String getEmail() {
+		return email;
+	}
 
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public int getSenha() {
+		return senha;
+	}
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
 }	
 		
 		
@@ -113,12 +124,10 @@ public class Cliente {
 		
 		
 		
-		
-		
-		
-		
 
 		
 			
+		
+		
 
 	
