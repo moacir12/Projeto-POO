191b164a88bf2e9771b1435c14c264320ba61fd1
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-22 20:34:00 -0300

    Update Ambiente.java

--- Ambiente.java
+++ Ambiente.java
@@ -1,46 +1,98 @@
-public class Ambiente{
-	
-	//Atributos
+import java.util.Scanner;
+
+//classe Ambiente 
+
+public abstract class Ambiente {
+
+	// Atributos
 	private String descricao;
 	private int capacidadeTotal;
 	private String endereço;
 	private String nome;
 	private String pontoDeReferencia;
 	private int vagasDeEstacionamento;
-	
+
 	Scanner entrada = new Scanner(System.in);
-	
-	// construtor 
-	
-	public Ambiente(String descricao, int capacidadeTotal,String endereço, 
-					String nome, String pontoDeReferencia, int vagasDeEstacionamento){
+
+	// construtor
+
+	public Ambiente(String descricao, int capacidadeTotal, String endereço,
+			String nome, String pontoDeReferencia, int vagasDeEstacionamento) {
 		this.descricao = descricao;
 		this.capacidadeTotal = capacidadeTotal;
 		this.endereço = endereço;
 		this.nome = nome;
 		this.pontoDeReferencia = pontoDeReferencia;
 		this.vagasDeEstacionamento = vagasDeEstacionamento;
-		
+
 	}
-	
-	// Dados do Ambiente
-	
-	String getDados(){
-		return "Dados do ambiente: \n Descrição do Ambiente: " + this.descricao + "\n Capacidade total: " + this.capacidadeTotal
-				+ "\nEndereço: " + this.endereço + "\nNome: " + this.nome + 
-				"\nPonto de Referencia: " + this.pontoDeReferencia + "\nQuantidade de vagas no estacionamento: " + this.vagasDeEstacionamento;
+
+	public String getDescricao() {
+		return descricao;
 	}
-	
-	//Metodos
-	void mostrarFotos(){
-		
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
-	
-	void adicionarAmbiente(){
-		
+
+	public int getCapacidadeTotal() {
+		return capacidadeTotal;
 	}
-	
-	void pesquisarAmbiente(){
-		
+
+	public void setCapacidadeTotal(int capacidadeTotal) {
+		this.capacidadeTotal = capacidadeTotal;
+	}
+
+	public String getEndereço() {
+		return endereço;
+	}
+
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPontoDeReferencia() {
+		return pontoDeReferencia;
+	}
+
+	public void setPontoDeReferencia(String pontoDeReferencia) {
+		this.pontoDeReferencia = pontoDeReferencia;
+	}
+
+	public int getVagasDeEstacionamento() {
+		return vagasDeEstacionamento;
+	}
+
+	public void setVagasDeEstacionamento(int vagasDeEstacionamento) {
+		this.vagasDeEstacionamento = vagasDeEstacionamento;
+	}
+
+	public Scanner getEntrada() {
+		return entrada;
+	}
+
+	public void setEntrada(Scanner entrada) {
+		this.entrada = entrada;
+	}
+
+	// Metodos
+	void mostrarFotos() {
+
+	}
+
+	void adicionarAmbiente() {
+
+	}
+
+	void pesquisarAmbiente() {
+
 	}
 }
