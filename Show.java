122f4d8c40460ffc25f557e800f2b09d48301e21
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-22 21:30:40 -0300

    Update Show.java

--- Show.java
+++ Show.java
@@ -1,35 +1,61 @@
 import java.util.Scanner;
 
-//Classe Show
-
-public class Show {
-	//Atributos
-	
-	private String nome;
-	private String artista;
-	private String estilo;
-	
-	Scanner entrada = new Scanner(System.in);
-		
-// construtor
-	public Esporte( String nome, String artista,String estilo){
-		this.nome = nome;
-		this.artista= artista;
-		this.estilo= estilo;
+class Show extends Evento{
+	public Show(String nome, String descricao, String dataEvento,
+			String horaEvento, String sinopse, String elenco) {
+		super(nome, descricao, dataEvento, horaEvento, sinopse, elenco);
 	}
 
-//  dados do Show
-		String getDados(){
-			return "\n\nTipo de show:\n\nNome : " + this.nome + "\nArtista: " + this.artista + "\n estilo:" + this.estilo;
-		}	
+
+
+
+
+	@Override
+	public String toString() {
+		return "Show [nome=" + nome + ", artista=" + artista + ", estilo="
+				+ estilo + "]";
+	}
+
+
+
+	//atributos
+	private String nome;
+	private String artista;
+	private String estilo;	
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getArtista() {
+		return artista;
+	}
+
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+
+	public String getEstilo() {
+		return estilo;
+	}
+
+	public void setEstilo(String estilo) {
+		this.estilo = estilo;
+	}
 
 	//Metodos
-	
 	void registrarShow(){
 		
 	}
 	
 	void pesquisarShow(){
-	
+		
 	}
 }
+	
