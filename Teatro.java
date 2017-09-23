a535c033f996decf8db64259e89e73f2a0508528
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-22 22:49:26 -0300

    Add files via upload

---
+++ Teatro.java
@@ -0,0 +1,18 @@
+class Teatro extends Ambiente {
+	public Teatro(String descricao, int capacidadeTotal, String endereço,
+			String nome, String pontoDeReferencia, int vagasDeEstacionamento) {
+		super(descricao, capacidadeTotal, endereço, nome, pontoDeReferencia,
+				vagasDeEstacionamento);
+	}
+
+	//Metodos
+	void adicionarTeatro(){
+		this.adicionarTeatro();
+		
+	}
+
+	void pesquisarTeatro(){
+		this.pesquisarTeatro();
+		
+	}
+}
