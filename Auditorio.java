171658de3f518ba6f44db72bd3dc9b619db620cd
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-22 22:52:27 -0300

    Add files via upload

---
+++ Auditorio.java
@@ -0,0 +1,18 @@
+class Auditorio extends Ambiente {
+	public Auditorio(String descricao, int capacidadeTotal, String endereço,
+			String nome, String pontoDeReferencia, int vagasDeEstacionamento) {
+		super(descricao, capacidadeTotal, endereço, nome, pontoDeReferencia,
+				vagasDeEstacionamento);
+	}
+
+	//Atributos
+	void adicionarAuditorio(){
+		this.adicionarAuditorio();
+	}
+
+	void pesquisarAuditorio(){
+		this.pesquisarAuditorio();
+	}
+	
+	
+}
