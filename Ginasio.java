8959e134e00aec03a9fb5e27072293e30bee3176
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-22 22:47:24 -0300

    Add files via upload

---
+++ Ginasio.java
@@ -0,0 +1,18 @@
+class Ginasio extends Ambiente {
+	public Ginasio(String descricao, int capacidadeTotal, String endereço,
+			String nome, String pontoDeReferencia, int vagasDeEstacionamento) {
+		super(descricao, capacidadeTotal, endereço, nome, pontoDeReferencia,
+				vagasDeEstacionamento);
+	}
+
+	//Atributos
+		void adicionarGinasio(){
+			this.adicionarGinasio();
+			
+		}
+
+		void pesquisarGinasio(){
+			this.pesquisarGinasio();
+		}
+		
+}
