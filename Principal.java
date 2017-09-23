18769d2d1189b1ebe3285326a69e6a963af915b7
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-22 23:53:21 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,32 +1,41 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 
-
-  //Classe que chamada os métodos 
- 
-
-
-public class Principal {
+public class Principal{
 
 	public static void main(String[] args) {
-		Cliente cl = new Cliente(null, null, null, 0, 0);
-		cadastrarCliente(cl); 
+		ArrayList<Cliente> clientes = new ArrayList<>();
+		ArrayList<Evento> eventos = new ArrayList<>();
+		@SuppressWarnings("unused")
+		ArrayList<Ambientes> ambientes = new ArrayList<>();
 		
-	}
-	private static void cadastrarCliente(Cliente cl){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("informe cpf");
-		String cpf = sc.next();
-		System.out.println("informe email");
-		String email = sc.next();
-		System.out.println("informe endereco");
-		String endereco = sc.next();
-		System.out.println("informe dataDeNascimento");
-		int dataDeNascimento = sc.nextInt();
-		System.out.println("informe telefone");
-		int telefone = sc.nextInt();
-		
-	}
-}
+  		Cliente c1 = new Cliente( "897.7855.655-06", "moacir@gmailcom", 
+				 "Rua Rio de Janeiro, 501", 2218764, 98765, "moacir", 12345);
 
 		
+		clientes.add(c1);
+		
+		//Ambiente a = new Ambiente("vila forro", 10 ,"Rua: sem nome","sem nome","sem ref",10000);
+		//ambientes.add(c1);
+		
+		//Esporte e = new Esporte("voley" , "Jose, Antonio, Carlos, Paulo");
+		
+		Evento ev = new Esporte("voley", "4 pessoas", "13/10/2017", "14hrs", "Jose, Antonio, Carlos, Paulo", null);
+		eventos.add(ev);
+		
+		for (Iterator<Evento> iterator = eventos.iterator(); iterator.hasNext();) {
+			System.out.println(iterator.toString());
+			
+		}
+			
+		}
+		//Filmes f = new Filme("Rambo", 18, "Ação, açao");
+		
+		//verificar dados:
+	     // System.out.println(c.getDados());
+		 // System.out.println(a.getDados());
+		//  System.out.println(e.getDados());
+		 // System.out.println(ev.getDados());
+		  //System.out.println(f.getDados());
+}
