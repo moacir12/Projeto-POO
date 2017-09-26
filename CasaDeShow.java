f8dc1874db9afade0a155bab293b4b5772fc20b2
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-26 19:08:58 -0300

    Add files via upload

---
+++ CasaDeShow.java
@@ -0,0 +1,17 @@
+class CasaDeShow extends Ambiente{
+	public CasaDeShow(String descricao, int capacidadeTotal, String endereço,
+			String nome, String pontoDeReferencia, int vagasDeEstacionamento) {
+		super(descricao, capacidadeTotal, endereço, nome, pontoDeReferencia,
+				vagasDeEstacionamento);
+	}
+
+	//Atributos
+	void adicionarCasaDeShow(){
+		this.adicionarCasaDeShow();
+				
+			}
+
+	void pesquisarCasaDeShow(){
+		this.adicionarCasaDeShow();
+			}
+}
