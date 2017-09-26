5d110ceb9e8acac8622bff3a6b2a688190184cd7
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-26 18:41:14 -0300

    Update Show.java

--- Show.java
+++ Show.java
@@ -1,19 +1,59 @@
-class CasaDeShow extends Ambiente{
-	public CasaDeShow(String descricao, int capacidadeTotal, String endereço,
-			String nome, String pontoDeReferencia, int vagasDeEstacionamento) {
-		super(descricao, capacidadeTotal, endereço, nome, pontoDeReferencia,
-				vagasDeEstacionamento);
+
+
+class Show extends Evento{
+	public Show(String nome, String descricao, String dataEvento,
+			String horaEvento, String sinopse, String elenco) {
+		super(nome, descricao, dataEvento, horaEvento, sinopse, null);
 	}
 
-	//Atributos
-	void adicionarCasaDeShow(){
-		this.adicionarCasaDeShow();
-				
-			}
 
-	void pesquisarCasaDeShow(){
-		this.adicionarCasaDeShow();
-			}
-}
-}
+
+
+	@Override
+	public String toString() {
+		return "Show [nome=" + nome + ", artista=" + artista + ", estilo="
+				+ estilo + "]";
+	}
+
+
+
+	//atributos
+	private String nome;
+	private String artista;
+	private String estilo;	
 	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getArtista() {
+		return artista;
+	}
+
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+
+	public String getEstilo() {
+		return estilo;
+	}
+
+	public void setEstilo(String estilo) {
+		this.estilo = estilo;
+	}
+
+	//Metodos
+	void registrarShow(){
+		
+	}
+	
+	void pesquisarShow(){
+		
+	}
+}
