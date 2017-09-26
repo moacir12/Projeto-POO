16727886d2856b96c6fd7c44eb52fe605ec862e5
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-26 18:33:22 -0300

    Update Ingresso.java

--- Ingresso.java
+++ Ingresso.java
@@ -1,7 +1,25 @@
 public class Ingresso {
-
-	// Atributos
+	//Atributos
+	
 	private String setor;
 	private int cadeiraNumerada;
-
+	public String getSetor() {
+		return setor;
+	}
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	public int getCadeiraNumerada() {
+		return cadeiraNumerada;
+	}
+	public void setCadeiraNumerada(int cadeiraNumerada) {
+		this.cadeiraNumerada = cadeiraNumerada;
+	}
+	@Override
+	public String toString() {
+		return "Ingresso [setor=" + setor + ", cadeiraNumerada="
+				+ cadeiraNumerada + "]";
+	}
+	
+	
 }
