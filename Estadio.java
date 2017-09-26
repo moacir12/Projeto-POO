d5f17fb05e3eada5bcb29e8ea50e7d1710b84789
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-26 19:18:35 -0300

    Add files via upload

---
+++ Estadio.java
@@ -0,0 +1,18 @@
+class Estadio extends Ambiente {
+	public Estadio(String descricao, int capacidadeTotal, String endereço,
+			String nome, String pontoDeReferencia, int vagasDeEstacionamento) {
+		super(descricao, capacidadeTotal, endereço, nome, pontoDeReferencia,
+				vagasDeEstacionamento);
+	}
+
+	//Atributos
+	void adicionarEstadio(){
+		this.adicionarEstadio();
+				
+	}
+
+	void pesquisarEstadio(){
+		this.pesquisarEstadio();
+				
+			}
+}
