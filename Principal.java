077649525c87eecc2fe80bfec04bf77b00ff1cbb
moacir12 <31716772+moacir12@users.noreply.github.com>
2017-09-26 18:58:36 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -7,7 +7,6 @@ public class Principal{
 	public static void main(String[] args) {
 		ArrayList<Cliente> clientes = new ArrayList<>();
 		ArrayList<Evento> eventos = new ArrayList<>();
-		@SuppressWarnings("unused")
 		ArrayList<Ambientes> ambientes = new ArrayList<>();
 		
   		Cliente c1 = new Cliente( "897.7855.655-06", "moacir@gmailcom", 
@@ -15,27 +14,20 @@ public class Principal{
 
 		
 		clientes.add(c1);
+		System.out.println(c1);
 		
-		//Ambiente a = new Ambiente("vila forro", 10 ,"Rua: sem nome","sem nome","sem ref",10000);
-		//ambientes.add(c1);
+		Ambiente a1 = new Ambiente("vila forro", 10 ,"Rua: sem nome","sem nome","sem ref",10000);
+		 ambientes.add(a1);
+		System.out.println(a1);	
 		
 		//Esporte e = new Esporte("voley" , "Jose, Antonio, Carlos, Paulo");
 		
 		Evento ev = new Esporte("voley", "4 pessoas", "13/10/2017", "14hrs", "Jose, Antonio, Carlos, Paulo", null);
 		eventos.add(ev);
 		
-		for (Iterator<Evento> iterator = eventos.iterator(); iterator.hasNext();) {
-			System.out.println(iterator.toString());
 			
 		}
 			
 		}
-		//Filmes f = new Filme("Rambo", 18, "Ação, açao");
 		
-		//verificar dados:
-	     // System.out.println(c.getDados());
-		 // System.out.println(a.getDados());
-		//  System.out.println(e.getDados());
-		 // System.out.println(ev.getDados());
-		  //System.out.println(f.getDados());
-}
+
